7e0e54116457d1f3b005d4feab8cf8ad291e0ae5
willbrid <ngaswilly77@gmail.com>
2023-04-04 03:57:10 +0100

    feat: finalize function concept

--- 7-functions/main.go
+++ 7-functions/main.go
@@ -62,6 +62,66 @@ func swapValuesWithTwoResults(first int, second int) (int, int) { // Fonction av
 	return second, first
 }
 
+func calcTaxWithCond(price float64) float64 {
+	if price > 100 {
+		return price * 0.2
+	}
+	return -1
+}
+
+func calcTaxWithTwoResults(price float64) (float64, bool) {
+	if price > 100 {
+		return price * 0.2, true
+	}
+	return 0, false
+}
+
+/**
+La fonction définit les résultats nommés total et taxe. Les deux sont des valeurs float64,
+ce qui signifie que l'on peut omettre le type de données du premier nom.
+**/
+func calcTotalPrice(products map[string]float64, minSpend float64) (total, tax float64) {
+	total = minSpend
+	for _, price := range products {
+		if taxAmount, due := calcTaxWithTwoResults(price); due {
+			total += taxAmount
+			tax += taxAmount
+		} else {
+			total += price
+		}
+	}
+	return
+}
+
+func calcTotalPriceWithOneParam(products map[string]float64) (count int, total float64) {
+	count = len(products)
+	for _, price := range products {
+		total += price
+	}
+
+	return
+}
+
+/**
+L'utilisation principale du mot-clé defer est d'appeler des fonctions qui libèrent des ressources, telles que la fermeture de fichiers ouverts ou
+de connexions HTTP. Sans le mot-clé defer, l'instruction qui libère la ressource doit apparaître à la fin d'une fonction, qui peut être composée de
+plusieurs instructions après la création et l'utilisation de la ressource. Le mot clé defer nous permet de regrouper les instructions qui créent,
+utilisent et libèrent la ressource ensemble.
+Le mot-clé defer peut être utilisé avec n'importe quel appel de fonction, et une seule fonction peut utiliser le mot-clé defer plusieurs fois.
+Juste avant le retour de la fonction, Go effectuera les appels programmés avec le mot-clé defer dans l'ordre dans lequel ils ont été définis.
+**/
+func calcTotalPriceWithDefer(products map[string]float64) (count int, total float64) {
+	fmt.Println("Function started")
+	defer fmt.Println("First defer call")
+	count = len(products)
+	for _, price := range products {
+		total += price
+	}
+	defer fmt.Println("Second defer call")
+	fmt.Println("Function about to return")
+	return
+}
+
 func main() {
 	fmt.Println("Hello, Functions")
 
@@ -109,4 +169,41 @@ func main() {
 	fmt.Println("Before calling function : ", val3, val4)
 	valResult1, valResult2 := swapValuesWithTwoResults(val3, val4)
 	fmt.Println("After calling function : ", valResult1, valResult2)
+
+	for product, price := range products {
+		tax := calcTaxWithCond(price)
+		if tax != -1 {
+			fmt.Println("Product: ", product, " - Tax:", tax)
+		} else {
+			fmt.Println("Product: ", product, "No tax due")
+		}
+	}
+
+	for product, price := range products {
+		taxAmount, taxDue := calcTaxWithTwoResults(price)
+		if taxDue {
+			fmt.Println("Product: ", product, " - Tax:", taxAmount)
+		} else {
+			fmt.Println("Product: ", product, "No tax due")
+		}
+	}
+
+	for product, price := range products {
+		if taxAmount, taxDue := calcTaxWithTwoResults(price); taxDue {
+			fmt.Println("Product: ", product, " - Tax:", taxAmount)
+		} else {
+			fmt.Println("Product: ", product, "No tax due")
+		}
+	}
+
+	total1, tax1 := calcTotalPrice(products, 10)
+	fmt.Println("Total 1:", total1, "Tax 1:", tax1)
+	total2, tax2 := calcTotalPrice(nil, 10)
+	fmt.Println("Total 2:", total2, "Tax 2:", tax2)
+
+	_, total3 := calcTotalPriceWithOneParam(products)
+	fmt.Println("Total : ", total3)
+
+	_, total4 := calcTotalPriceWithDefer(products)
+	fmt.Println("Total : ", total4)
 }
