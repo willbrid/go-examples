bdc71230d1c6a0df27901628306b61be990ce0ea
willbrid <ngaswilly77@gmail.com>
2023-05-02 04:07:53 +0100

    feat: add closing channel instruction

--- 13-concurrency/main.go
+++ 13-concurrency/main.go
@@ -36,5 +36,34 @@ func main() {
 	fmt.Println("Méthode CalcStoreTotal4")
 	CalcStoreTotal4(Products)
 
+	// Envoi et réception d'un nombre inconnu de valeurs
+	/**
+	L'opérateur de réception peut être utilisé pour obtenir deux valeurs.
+	La première valeur est affectée à la valeur reçue du canal et la deuxième valeur indique si le canal est fermé.
+	**/
+	var dispatchChannel chan DispatchNotification = make(chan DispatchNotification)
+	var details DispatchNotification
+	var open bool
+
+	go DispatchOrders(dispatchChannel)
+	for {
+		// On utilise cette instruction parce que la fonction close est appelée au niveau de la méthode DispatchOrders
+		if details, open = <-dispatchChannel; open {
+			fmt.Println("Dispatch to ", details.Customer, " : ", details.Quantity, " x ", details.Product.Name)
+		} else {
+			fmt.Println("Channel has been closed")
+			break
+		}
+	}
+
+	/**
+	Une boucle for peut être utilisée avec le mot-clé range pour énumérer les valeurs envoyées via un canal,
+	permettant aux valeurs d'être reçues plus facilement et mettant fin à la boucle lorsque le canal est fermé
+	**/
+	for details = range dispatchChannel {
+		fmt.Println("Dispatch to ", details.Customer, " : ", details.Quantity, " x ", details.Product.Name)
+	}
+	fmt.Println("Channel has been closed")
+
 	fmt.Println("main function complete")
 }
---
+++ 13-concurrency/orderdispatch.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type DispatchNotification struct {
+	Customer string
+	*Product
+	Quantity int
+}
+
+var Customers []string = []string{"Alice", "Bob", "Charlie", "Dora"}
+
+/**
+La fonction intégrée close accepte un canal comme argument et est utilisée pour indiquer qu'aucune autre valeur
+ne sera envoyée via le canal. Les récepteurs peuvent vérifier si un canal est fermé lorsqu'ils demandent une valeur
+**/
+
+func DispatchOrders(channel chan DispatchNotification) {
+	var orderCount int
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	orderCount = rand.Intn(3) + 2
+	fmt.Println("Order count : ", orderCount)
+
+	for i := 0; i < orderCount; i++ {
+		channel <- DispatchNotification{
+			Customer: Customers[rand.Intn(len(Customers)-1)],
+			Quantity: rand.Intn(10),
+			Product:  ProductList[rand.Intn(len(ProductList)-1)],
+		}
+	}
+	close(channel)
+}
