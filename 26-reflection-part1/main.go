aa7968910ecc7c6cf38e77eb8e4ebab6a5a76cb2
willbrid <ngaswilly77@gmail.com>
2023-07-01 06:26:20 +0100

    feat: add identify type and obtenaing Value fro reflect.Value

--- 26-reflection-part1/main.go
+++ 26-reflection-part1/main.go
@@ -50,6 +50,31 @@ Les constantes liées à la méthode reflect.Type.kind() :
 - Interface : cette valeur désigne une interface.
 - Ptr : cette valeur désigne un pointeur
 - Uintptr : cette valeur indique un pointeur non sécurisé
+
+Les méthodes de la classe reflect.Value :
+- Kind() : cette méthode renvoie le genre du type de la valeur.
+- Type() : cette méthode renvoie le type de la valeur.
+- IsNil() : cette méthode renvoie true si la valeur est nil. Cette méthode paniquera si la valeur sous-jacente n'est pas une fonction, une interface,
+            un pointeur, une tranche ou un canal.
+- IsZero() : cette méthode renvoie true si la valeur sous-jacente est la valeur zéro de son type.
+- Bool() : cette méthode renvoie la valeur bool sous-jacente. La méthode panique si le Kind de la valeur sous-jacente n'est pas Bool.
+- Bytes() : cette méthode renvoie la valeur []byte sous-jacente. La méthode panique si la valeur sous-jacente n'est pas une tranche d'octet.
+- Int() : cette méthode renvoie la valeur sous-jacente sous la forme d'un int64. La méthode panique si le Kind de la valeur sous-jacente
+          n'est pas Int, Int8, Int16, Int32 ou Int64.
+- Uint() : cette méthode renvoie la valeur sous-jacente sous la forme d'un uint64. La méthode panique si le Kind de la valeur sous-jacente
+           n'est pas Uint, Uint8, Uint16, Uint32 ou Uint64.
+- Float() : cette méthode renvoie la valeur sous-jacente sous la forme d'un float64. La méthode panique si le Kind de la valeur sous-jacente
+            n'est pas Float32 ou Float64.
+- String() : cette méthode renvoie la valeur sous-jacente sous forme de chaîne si le Kind de la valeur est String. Pour les autres
+             valeurs Kind, cette méthode renvoie la chaîne <T Value> où T est le type sous-jacent, tel que <int Value>.
+- Elem() : cette méthode renvoie la valeur à laquelle un pointeur se réfère. Cette méthode peut également être utilisée avec des interfaces.
+           Cette méthode panique si le Kind de la valeur sous-jacente n'est pas Ptr.
+- IsValid() : cette méthode renvoie false si Value est la valeur zéro, créée en tant que Value{} plutôt qu'obtenue à l'aide de ValueOf, par exemple.
+              Cette méthode ne concerne pas les valeurs reflétées qui sont la valeur zéro de leur type réfléchi.
+			  Si cette méthode renvoie false, toutes les autres méthodes Value paniqueront.
+- Interface() : cette méthode renvoie la valeur sous-jacente à l'aide de l'interface vide. Cette méthode paniquera si elle est utilisée
+                sur des champs de structure non exportés.
+- CanInterface() : cette méthode retourne true si la méthode Interface peut être utilisée sans paniquer.
 **/
 
 type Payment struct {
@@ -128,6 +153,113 @@ func printDetailsWithReflectionAndTypePath(values ...interface{}) {
 	}
 }
 
+func printDetailsWithReflectionAndReflectValueStruct(values ...interface{}) {
+	for _, elem := range values {
+		elemValue := reflect.ValueOf(elem)
+		switch elemValue.Kind() {
+		case reflect.Bool:
+			var val bool = elemValue.Bool()
+			Printfln("Bool: %v", val)
+		case reflect.Int:
+			var val int64 = elemValue.Int()
+			Printfln("Int: %v", val)
+		case reflect.Float32, reflect.Float64:
+			var val float64 = elemValue.Float()
+			Printfln("Float: %v", val)
+		case reflect.String:
+			var val string = elemValue.String()
+			Printfln("String: %v", val)
+		case reflect.Ptr:
+			var val reflect.Value = elemValue.Elem()
+			if val.Kind() == reflect.Int {
+				Printfln("Pointer to Int : %v", val.Int())
+			}
+		default:
+			Printfln("Other: %v", elemValue.String())
+		}
+	}
+}
+
+/*
+*
+Cette technique commence par une valeur nulle et la convertit en un pointeur vers une valeur int, qui est ensuite transmise à la fonction TypeOf
+pour obtenir un Type pouvant être utilisé dans les comparaisons. cette approche évite d'avoir à définir une variable juste pour obtenir son Type.
+*
+*/
+var intPtrType = reflect.TypeOf((*int)(nil))
+var byteSliceType = reflect.TypeOf([]byte(nil))
+
+func printDetailsWithReflectionByComparingType1(values ...interface{}) {
+	for _, elem := range values {
+		elemValue := reflect.ValueOf(elem)
+		elemType := reflect.TypeOf(elem)
+		if elemType == intPtrType {
+			Printfln("Pointer to Int: %v", elemValue.Elem().Int())
+		} else {
+			switch elemValue.Kind() {
+			case reflect.Bool:
+				var val bool = elemValue.Bool()
+				Printfln("Bool: %v", val)
+			case reflect.Int:
+				var val int64 = elemValue.Int()
+				Printfln("Int: %v", val)
+			case reflect.Float32, reflect.Float64:
+				var val float64 = elemValue.Float()
+				Printfln("Float: %v", val)
+			case reflect.String:
+				var val string = elemValue.String()
+				Printfln("String: %v", val)
+			default:
+				Printfln("Other: %v", elemValue.String())
+			}
+		}
+	}
+}
+
+func printDetailsWithReflectionByComparingType2(values ...interface{}) {
+	for _, elem := range values {
+		elemValue := reflect.ValueOf(elem)
+		elemType := reflect.TypeOf(elem)
+		if elemType == intPtrType {
+			Printfln("Pointer to Int: %v", elemValue.Elem().Int())
+		} else if elemType == byteSliceType {
+			Printfln("Byte slice: %v", elemValue.Bytes())
+		} else {
+			switch elemValue.Kind() {
+			case reflect.Bool:
+				var val bool = elemValue.Bool()
+				Printfln("Bool: %v", val)
+			case reflect.Int:
+				var val int64 = elemValue.Int()
+				Printfln("Int: %v", val)
+			case reflect.Float32, reflect.Float64:
+				var val float64 = elemValue.Float()
+				Printfln("Float: %v", val)
+			case reflect.String:
+				var val string = elemValue.String()
+				Printfln("String: %v", val)
+			default:
+				Printfln("Other: %v", elemValue.String())
+			}
+		}
+	}
+}
+
+/*
+*
+La fonction selectValue sélectionne une valeur dans une tranche sans connaître le type d'élément de la tranche. La valeur est extraite de la tranche à
+l'aide de la méthode Index. Ce qui est important, c'est que la méthode Index renvoie une valeur, qui n'est utile que pour le code qui utilise la réflexion.
+La méthode Interface est utilisée pour obtenir une valeur qui peut être utilisée comme résultat de la fonction.
+*
+*/
+func selectValue(data interface{}, index int) (result interface{}) {
+	dataVal := reflect.ValueOf(data)
+	if dataVal.Kind() == reflect.Slice {
+		result = dataVal.Index(index).Interface()
+	}
+	return
+}
+
 func main() {
 	product1 := Product{
 		Name: "Kayak", Category: "Watersports", Price: 279,
@@ -153,4 +285,34 @@ func main() {
 	customer4 := Customer{Name: "Alice", City: "New Yorl"}
 	payment4 := Payment{Currency: "USD", Amount: 100.50}
 	printDetailsWithReflectionAndTypePath(product4, customer4, payment4, 10, true)
+
+	product5 := Product{
+		Name: "Kayak", Category: "Watersports", Price: 279,
+	}
+	customer5 := Customer{Name: "Alice", City: "New Yorl"}
+	payment5 := Payment{Currency: "USD", Amount: 100.50}
+	number5 := 100
+	printDetailsWithReflectionAndReflectValueStruct(product5, customer5, payment5, true, 10, &number5, "Alice", 23.30)
+
+	product6 := Product{
+		Name: "Kayak", Category: "Watersports", Price: 279,
+	}
+	customer6 := Customer{Name: "Alice", City: "New Yorl"}
+	payment6 := Payment{Currency: "USD", Amount: 100.50}
+	number6 := 100
+	printDetailsWithReflectionByComparingType1(product6, customer6, payment6, true, 10, &number6, "Alice", 23.30)
+
+	product7 := Product{
+		Name: "Kayak", Category: "Watersports", Price: 279,
+	}
+	customer7 := Customer{Name: "Alice", City: "New Yorl"}
+	payment7 := Payment{Currency: "USD", Amount: 100.50}
+	number7 := 100
+	slice7 := []byte("Alice")
+	printDetailsWithReflectionByComparingType2(product7, customer7, payment7, true, 10, &number7, "Alice", 23.30, slice7)
+
+	names := []string{"Alice", "Bob", "Charlie"}
+	// Conversion du résultat en string
+	val := selectValue(names, 1).(string)
+	Printfln("Selected: %v", val)
 }
