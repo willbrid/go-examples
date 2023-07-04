9b89c75d7a35c982af9cbcc5ea005f6cc0ccc871
willbrid <ngaswilly77@gmail.com>
2023-07-04 06:10:56 +0100

    feat: add working with map reflection

--- 27-reflection-part2/main.go
+++ 27-reflection-part2/main.go
@@ -6,27 +6,58 @@ import (
 )
 
 /**
-La fonction et la méthode du package reflect pour les pointeurs :
+La fonction et la méthode du package reflect pour les pointeurs ou les maps :
 - PtrTo(type) : cette fonction renvoie un Type (reflect.Type) qui est un pointeur vers le Type reçu en argument.
 - Elem() : --- cette méthode, qui est appelée sur un type pointeur (reflect.Type), renvoie le Type sous-jacent.
            Cette méthode panique lorsqu'elle est utilisée sur des types non pointeurs.
-		   --- Cette méthode renvoie le Type pour les éléments de tableau ou de tranche.
+		   --- Cette méthode renvoie reflect.Type pour les éléments de tableau ou de tranche.
+		   --- Cette méthode renvoie reflect.Type pour les valeurs de map.
 - Len() : cette méthode renvoie la longueur d'un type tableau. Cette méthode paniquera si elle est appelée sur d'autres types, y compris les tranches.
 - ArrayOf(len, type) : cette fonction renvoie un Type qui décrit un tableau avec la taille et le type d'élément spécifiés.
 - SliceOf(type) : cette fonction renvoie un Type qui décrit un tableau avec le type d'élément spécifié.
+- Key() : cette méthode renvoie le Type (reflect.Type) des clés de la map.
 
-Les méthodes de reflect.Value pour travailler avec des types de pointeur, des types array ou slice :
+Les méthodes de reflect.Value pour travailler avec des types de pointeur, des types array ou slice, des types map :
 - Addr() : cette méthode renvoie une valeur qui est un pointeur vers la valeur sur laquelle elle est appelée.
            Cette méthode panique si la méthode CanAddr renvoie false.
 - CanAddr() : cette méthode renvoie vrai si la valeur peut être utilisée avec la méthode Addr.
 - Elem() : cette méthode suit un pointeur et renvoie sa valeur. Cette méthode panique si elle est appelée sur une valeur non pointeur.
 - Index(index) : cette méthode renvoie une valeur qui représente l'élément à l'index spécifié.
 - Len() : cette méthode renvoie la longueur du tableau ou de la tranche.
+          pour les maps, cette méthode renvoie le nombre de paires clé-valeur contenues dans une map.
 - Cap() : cette méthode renvoie la capacité du tableau ou de la tranche.
 - SetLen() : cette méthode définit la longueur d'une tranche. Elle ne peut pas être utilisé sur des tableaux.
 - SetCap() : cette méthode définit la capacité d'une tranche. Elle ne peut pas être utilisé sur des tableaux.
 - Slice(lo, hi) : cette méthode crée une nouvelle tranche avec les valeurs low et high spécifiées.
 - Slice3(lo, hi, max) : cette méthode crée une nouvelle tranche avec les valeurs low, high et max spécifiées.
+- MapKeys() : cette méthode renvoie un []reflect.Value, contenant les clés de la map.
+- MapIndex(key) : cette méthode renvoie la valeur qui correspond à la clé spécifiée, qui est également exprimée sous forme de reflect.Value.
+                  La valeur zéro est renvoyée si la map ne contient pas la clé spécifiée, qui peut être détectée
+				  en appelant la méthode IsValid, qui renverra false.
+- MapRange() : cette méthode renvoie un *MapIter, qui permet d'itérer le contenu de la map.
+- SetMapIndex(key, val) : cette méthode définit la clé et la valeur spécifiées, toutes deux exprimées à l'aide de l'interface reflect.Value.
+
+Fonctions d'ajout d'éléments aux tranches du package reflect -> ces fonctions acceptent les arguments reflect.Type ou reflect.Value :
+- MakeSlice(type, len, cap) : cette fonction crée un reflect.Value qui reflète une nouvelle tranche, en utilisant un reflect.Type pour désigner
+                              le type d'élément et avec la longueur et la capacité spécifiées.
+- Append(sliceVal, ...val) : cette fonction ajoute une ou plusieurs valeurs à la tranche spécifiée, qui sont toutes exprimées à l'aide
+                             de l'interface Value. Le résultat est la tranche modifiée. La fonction panique lorsqu'elle est utilisée sur
+							 tout type de reflect.Type autre qu'une tranche ou si les types des valeurs ne correspondent pas au type d'élément de tranche.
+- AppendSlice(sliceVal, sliceVal) : cette fonction ajoute une tranche à une autre. La fonction panique si l'une des valeurs ne représente pas
+                                    une tranche ou si les types de tranche ne sont pas compatibles.
+- Copy(dst, src) : cette fonction copie les éléments de la tranche ou du tableau reflété par la valeur src vers la tranche ou le tableau reflété
+                   par la valeur dst. Les éléments sont copiés jusqu'à ce que la tranche de destination soit pleine ou que tous
+				   les éléments source aient été copiés. La source et la destination doivent avoir le même type d'élément.
+
+La fonction reflect pour créer des types de map :
+- MapOf(keyType, valueType) : cette fonction renvoie un nouveau reflect.Type qui reflète le type de mappage avec les types de clé et de valeur spécifiés,
+                               tous deux décrits à l'aide d'un reflect.Type.
+
+Les méthodes définies par la classe reflect.MapIter :
+- Next() : cette méthode passe à la paire clé-valeur suivante dans la map. Le résultat de cette méthode est un booléen indiquant
+          s'il existe d'autres paires clé-valeur à lire. Cette méthode doit être appelée avant la méthode Key ou Value.
+- Key() : cette méthode renvoie la valeur représentant la clé de map à la position actuelle.
+- Value() : cette méthode renvoie la valeur représentant la valeur de la map à la position actuelle.
 **/
 
 func createPointerType(t reflect.Type) reflect.Type {
@@ -106,6 +137,85 @@ func enumerateStrings(arrayOrSlice interface{}) {
 	}
 }
 
+/*
+*
+La fonction findAndSplit énumère la tranche, en recherchant l'élément spécifié, ce qui est fait à l'aide de la méthode Interface,
+qui permet de comparer les éléments de la tranche sans avoir à traiter de types spécifiques. Une fois l'élément cible localisé,
+la méthode Slice est utilisée pour créer et renvoyer une nouvelle tranche.
+*
+*/
+func findAndSplit(slice interface{}, target interface{}) interface{} {
+	sliceVal := reflect.ValueOf(slice)
+	targetType := reflect.TypeOf(target)
+	if sliceVal.Kind() == reflect.Slice && sliceVal.Type().Elem() == targetType {
+		for i := 0; i < sliceVal.Len(); i++ {
+			if sliceVal.Index(i).Interface() == target {
+				return sliceVal.Slice(0, i+1)
+			}
+		}
+	}
+
+	return slice
+}
+
+/*
+*
+La fonction pickValues crée une nouvelle tranche à l'aide de reflect.Type à partir d'une tranche existante et utilise
+la fonction Append pour ajouter des valeurs à la nouvelle tranche.
+*
+*/
+func pickValues(slice interface{}, indices ...int) interface{} {
+	sliceVal := reflect.ValueOf(slice)
+	if sliceVal.Kind() == reflect.Slice {
+		newSlice := reflect.MakeSlice(sliceVal.Type(), 0, 10)
+		for _, index := range indices {
+			newSlice = reflect.Append(newSlice, sliceVal.Index(index))
+		}
+		return newSlice
+	}
+
+	return nil
+}
+
+func describeMap(m interface{}) {
+	mapType := reflect.TypeOf(m)
+	if mapType.Kind() == reflect.Map {
+		Printfln("Key type: %v, Val type: %v", mapType.Key(), mapType.Elem())
+	} else {
+		Printfln("Not a map")
+	}
+}
+
+func printMapContents(m interface{}) {
+	mapValue := reflect.ValueOf(m)
+	if mapValue.Kind() == reflect.Map {
+		for _, keyVal := range mapValue.MapKeys() {
+			reflectedVal := mapValue.MapIndex(keyVal)
+			Printfln("Map Key : %v, Value : %v", keyVal, reflectedVal)
+		}
+	} else {
+		Printfln("Not a map")
+	}
+}
+
+/*
+*
+Il est important d'appeler la méthode Next avant d'appeler les méthodes Key et Value et d'éviter d'appeler ces méthodes
+lorsque la méthode Next renvoie false.
+*
+*/
+func printMapContentsWithMapIter(m interface{}) {
+	mapValue := reflect.ValueOf(m)
+	if mapValue.Kind() == reflect.Map {
+		iter := mapValue.MapRange()
+		for iter.Next() {
+			Printfln("Map Key : %v, Value : %v", iter.Key(), iter.Value())
+		}
+	} else {
+		Printfln("Not a map")
+	}
+}
+
 func main() {
 	name1 := "Alice"
 	t := reflect.TypeOf(name1)
@@ -142,4 +252,28 @@ func main() {
 	Printfln("Modified array : %v", array4)
 	enumerateStrings(slice4)
 	enumerateStrings(array4)
+
+	name5 := "Alice"
+	city5 := "London"
+	hobby5 := "Running"
+	slice5 := []string{name5, city5, hobby5}
+	Printfln("Strings: %v", findAndSplit(slice5, "London"))
+	numbers5 := []int{1, 3, 4, 5, 7}
+	Printfln("Numbers: %v", findAndSplit(numbers5, 4))
+
+	name6 := "Alice"
+	city6 := "London"
+	hobby6 := "Running"
+	slice6 := []string{name6, city6, hobby6, "Bob", "Paris", "Soccer"}
+	picked := pickValues(slice6, 0, 3, 5)
+	Printfln("Picked values: %v", picked)
+
+	pricesMap := map[string]float64{
+		"Kayak":       279,
+		"Lifejacket":  48.95,
+		"Soccer Ball": 19.50,
+	}
+	describeMap(pricesMap)
+	printMapContents(pricesMap)
+	printMapContentsWithMapIter(pricesMap)
 }
