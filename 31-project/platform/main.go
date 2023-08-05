03b33cc0ba510fc22468d127198a64d8842f790d
willbrid <ngaswilly77@gmail.com>
2023-08-05 19:24:34 +0100

    integrate the startup server

--- 31-project/platform/main.go
+++ 31-project/platform/main.go
@@ -1,45 +1,11 @@
 package main
 
 import (
-	"platform/config"
-	"platform/logging"
+	"platform/placeholder"
 	"platform/services"
 )
 
-func writeMessage(logger logging.Logger, cfg config.Configuration) {
-	section, ok := cfg.GetSection("main")
-	if ok {
-		message, ok := section.GetString("message")
-		if ok {
-			logger.Info(message)
-		} else {
-			logger.Panic("Cannot find configuration setting")
-		}
-	} else {
-		logger.Panic("Config section not found")
-	}
-}
-
 func main() {
 	services.RegisterDefaultServices()
-
-	/**
-	var cfg config.Configuration
-	services.GetService(&cfg)
-
-	var logger logging.Logger = logging.NewDefaultLogger(cfg)
-	services.GetService(&logger)
-	writeMessage(logger, cfg)
-	**/
-
-	services.Call(writeMessage)
-
-	val := struct {
-		message string
-		logging.Logger
-	}{
-		message: "Hello from the struct",
-	}
-	services.Populate(&val)
-	val.Logger.Debug(val.message)
+	placeholder.Start()
 }
