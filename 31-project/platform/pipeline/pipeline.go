7f98fdf76f2beeb1ebf719d3b0eb1f2a9ec11ead
willbrid <ngaswilly77@gmail.com>
2023-08-09 02:21:01 +0100

    feat: Adding Support for Services in the pipeline.go

--- 31-project/platform/pipeline/pipeline.go
+++ 31-project/platform/pipeline/pipeline.go
@@ -2,6 +2,8 @@ package pipeline
 
 import (
 	"net/http"
+	"platform/services"
+	"reflect"
 )
 
 type RequestPipeline func(*ComponentContext)
@@ -13,16 +15,40 @@ func CreatePipeline(components ...MiddlewareComponent) RequestPipeline {
 
 	for i := len(components) - 1; i >= 0; i-- {
 		currentComponent := components[i]
+		services.Populate(currentComponent)
 		nextFunc := f
-		f = func(context *ComponentContext) {
-			if context.error == nil {
-				currentComponent.ProcessRequest(context, nextFunc)
+		if servComp, ok := currentComponent.(ServicesMiddlwareComponent); ok {
+			f = createServiceDependentFunction(currentComponent, nextFunc)
+			servComp.Init()
+		} else if stdComp, ok := currentComponent.(MiddlewareComponent); ok {
+			f = func(context *ComponentContext) {
+				if context.error == nil {
+					stdComp.ProcessRequest(context, nextFunc)
+				}
 			}
+			stdComp.Init()
+		} else {
+			panic("Value is not a middleware component")
 		}
-		currentComponent.Init()
 	}
 
 	return f
+}
+
+func createServiceDependentFunction(component interface{}, nextFunc RequestPipeline) RequestPipeline {
+	method := reflect.ValueOf(component).MethodByName("ProcessRequestWithServices")
+	if method.IsValid() {
+		return func(context *ComponentContext) {
+			if context.error == nil {
+				_, err := services.CallForContext(context.Request.Context(), method.Interface(), context, nextFunc)
+				if err != nil {
+					context.Error(err)
+				}
+			}
+		}
+	} else {
+		panic("No ProcessRequestWithServices method defined")
+	}
 }
 
 func (pl RequestPipeline) ProcessRequest(req *http.Request, resp http.ResponseWriter) error {
