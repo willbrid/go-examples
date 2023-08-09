1f842c75824778cddd008af33fb9f67cdc75c908
willbrid <ngaswilly77@gmail.com>
2023-08-09 02:46:44 +0100

    feat: Creating the Template Service in the services_default.go

--- 31-project/platform/services/services_default.go
+++ 31-project/platform/services/services_default.go
@@ -3,6 +3,7 @@ package services
 import (
 	"platform/config"
 	"platform/logging"
+	"platform/templates"
 )
 
 func RegisterDefaultServices() {
@@ -13,9 +14,16 @@ func RegisterDefaultServices() {
 		}
 		return
 	})
+
 	err = AddSingleton(func(appconfig config.Configuration) logging.Logger {
 		return logging.NewDefaultLogger(appconfig)
 	})
+
+	err = AddSingleton(func(c config.Configuration) templates.TemplateExecutor {
+		templates.LoadTemplates(c)
+		return &templates.LayoutTemplateProcessor{}
+	})
+
 	if err != nil {
 		panic(err)
 	}
