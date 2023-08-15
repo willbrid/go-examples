d642012d7eca2f9de314c5e70e8534621318d352
willbrid <ngaswilly77@gmail.com>
2023-08-15 10:23:04 +0100

    add a new day handler, add to the startup router and update simple_message.html file

--- 31-project/platform/placeholder/startup.go
+++ 31-project/platform/placeholder/startup.go
@@ -17,6 +17,7 @@ func createPipeline() pipeline.RequestPipeline {
 		&basic.StaticFileComponent{},
 		handling.NewRouter(
 			handling.HandlerEntry{"", NameHandler{}},
+			handling.HandlerEntry{"", DayHandler{}},
 		),
 		// &SimpleMessageComponent{},
 	)
