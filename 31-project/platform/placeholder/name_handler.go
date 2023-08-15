076b4d4b1cf450acddf9ab35b542fcd36c354a0e
willbrid <ngaswilly77@gmail.com>
2023-08-15 10:36:46 +0100

    feat: generating a URL in the name_handler.go file

--- 31-project/platform/placeholder/name_handler.go
+++ 31-project/platform/placeholder/name_handler.go
@@ -3,6 +3,7 @@ package placeholder
 import (
 	"fmt"
 	"platform/http/actionresults"
+	"platform/http/handling"
 	"platform/logging"
 )
 
@@ -10,6 +11,7 @@ var names = []string{"Alice", "Bob", "Charlie", "Dora"}
 
 type NameHandler struct {
 	logging.Logger
+	handling.URLGenerator
 }
 
 func (n NameHandler) GetName(i int) actionresults.ActionResult {
@@ -40,9 +42,22 @@ func (n NameHandler) PostName(new NewName) actionresults.ActionResult {
 	} else {
 		names = append(names, new.Name)
 	}
-	return actionresults.NewRedirectAction("/names")
+	return n.redirectOrError(NameHandler.GetNames)
+}
+
+func (n NameHandler) GetRedirect() actionresults.ActionResult {
+	return n.redirectOrError(NameHandler.GetNames)
 }
 
 func (n NameHandler) GetJsonData() actionresults.ActionResult {
 	return actionresults.NewJsonAction(names)
+}
+
+func (n NameHandler) redirectOrError(handler interface{}, data ...interface{}) actionresults.ActionResult {
+	url, err := n.GenerateUrl(handler)
+	if err == nil {
+		return actionresults.NewRedirectAction(url)
+	} else {
+		return actionresults.NewErrorAction(err)
+	}
 }
