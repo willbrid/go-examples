f2abe2d756a6e9076fef764f9c9fc177012c8018
willbrid <ngaswilly77@gmail.com>
2023-08-15 10:19:59 +0100

    feat: Updating Result Execution in the request_dispatch File

--- 31-project/platform/http/handling/request_dispatch.go
+++ 31-project/platform/http/handling/request_dispatch.go
@@ -56,7 +56,10 @@ func (router *RouterComponent) invokeHandler(route Route, rawParams []string, co
 
 		if len(result) > 0 {
 			if action, ok := result[0].Interface().(actionresults.ActionResult); ok {
-				err = services.PopulateForContext(context.Context(), action)
+				invoker := createInvokehandlerFunc(context.Context(), router.routes)
+				err = services.PopulateForContextWithExtras(context.Context(), action, map[reflect.Type]reflect.Value{
+					reflect.TypeOf(invoker): reflect.ValueOf(invoker),
+				})
 				if err == nil {
 					err = action.Execute(&actionresults.ActionContext{
 						context.Context(),
