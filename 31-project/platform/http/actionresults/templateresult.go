bb84e7d6bc57af833e9e3361628138397d1a5937
willbrid <ngaswilly77@gmail.com>
2023-08-15 10:07:27 +0100

    update: Updating Template Execution in the layout_executor and adding an argument in the templateresult

--- 31-project/platform/http/actionresults/templateresult.go
+++ 31-project/platform/http/actionresults/templateresult.go
@@ -12,8 +12,9 @@ type TemplateActionResult struct {
 	templateName string
 	data         interface{}
 	templates.TemplateExecutor
+	templates.InvokeHandlerFunc
 }
 
 func (action *TemplateActionResult) Execute(ctx *ActionContext) error {
-	return action.TemplateExecutor.ExecTemplate(ctx.ResponseWriter, action.templateName, action.data)
+	return action.TemplateExecutor.ExecTemplateWithFunc(ctx.ResponseWriter, action.templateName, action.data, action.InvokeHandlerFunc)
 }
