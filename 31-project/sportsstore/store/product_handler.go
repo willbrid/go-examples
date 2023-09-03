632b24fc1152c2c493bc8ce42b2d5240fcde90d2
willbrid <ngaswilly77@gmail.com>
2023-09-03 21:51:15 +0100

    feat: create product handler and define product list template

---
+++ 31-project/sportsstore/store/product_handler.go
@@ -0,0 +1,20 @@
+package store
+
+import (
+	"platform/http/actionresults"
+	"sportsstore/models"
+)
+
+type ProductHandler struct {
+	Repository models.Repository
+}
+
+type ProductTemplateContext struct {
+	Products []models.Product
+}
+
+func (handler ProductHandler) GetProducts() actionresults.ActionResult {
+	return actionresults.NewTemplateAction("product_list.html", ProductTemplateContext{
+		Products: handler.Repository.GetProducts(),
+	})
+}
