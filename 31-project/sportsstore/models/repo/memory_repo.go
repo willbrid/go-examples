21adcece81377fd869360cb4b0192bf5c4eda815
willbrid <ngaswilly77@gmail.com>
2023-09-04 06:09:10 +0100

    feat: add GetProductPageCategory method for category filtering

--- 31-project/sportsstore/models/repo/memory_repo.go
+++ 31-project/sportsstore/models/repo/memory_repo.go
@@ -50,3 +50,18 @@ func getPage(src []models.Product, page, pageSize int) []models.Product {
 	}
 	return []models.Product{}
 }
+
+func (repo *MemoryRepo) GetProductPageCategory(category int, page, pageSize int) (products []models.Product, totalAvailable int) {
+	if category == 0 {
+		return repo.GetProductPage(page, pageSize)
+	} else {
+		filteredProducts := make([]models.Product, 0, len(repo.products))
+		for _, p := range repo.products {
+			if p.Category.ID == category {
+				filteredProducts = append(filteredProducts, p)
+			}
+		}
+
+		return getPage(filteredProducts, page, pageSize), len(filteredProducts)
+	}
+}
--- 31-project/sportsstore/models/repository.go
+++ 31-project/sportsstore/models/repository.go
@@ -4,6 +4,7 @@ type Repository interface {
 	GetProduct(id int) Product
 	GetProducts() []Product
 	GetProductPage(page, pageSize int) (products []Product, totalAvailable int)
+	GetProductPageCategory(categoryId int, page, pageSize int) (products []Product, totalAvailable int)
 	GetCategories() []Category
 	Seed()
 }
