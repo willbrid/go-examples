13efe007cd798714660840b4f6b0ec2256f08cf9
willbrid <ngaswilly77@gmail.com>
2024-10-07 11:23:58 +0200

    test: refactor test func and add subtest

--- 33-tdd/calculator-project/calculator/engine_test.go
+++ 33-tdd/calculator-project/calculator/engine_test.go
@@ -37,48 +37,104 @@ func TestAdd(t *testing.T) {
 
 	// Arrange
 	e := calculator.Engine{}
-	x, y := 2.5, 3.5
-	want := 6.0
 
-	got := e.Add(x, y)
+	actAssert := func(x, y, want float64) {
+		got := e.Add(x, y)
 
-	if got != want {
-		t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
+		if got != want {
+			t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
+		}
 	}
+
+	t.Run("positive input", func(t *testing.T) {
+		x, y := 2.5, 3.5
+		want := 6.0
+
+		actAssert(x, y, want)
+	})
+
+	t.Run("negative input", func(t *testing.T) {
+		x, y := -2.5, -3.5
+		want := -6.0
+
+		actAssert(x, y, want)
+	})
 }
 
 func TestSubtract(t *testing.T) {
 	e := calculator.Engine{}
-	x, y := 7.0, 5.0
-	want := 2.0
 
-	got := e.Subtract(x, y)
+	actAssert := func(x, y, want float64) {
+		got := e.Subtract(x, y)
 
-	if got != want {
-		t.Errorf("Subtract(%.2f,%.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
+		if got != want {
+			t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
+		}
 	}
+
+	t.Run("positive input", func(t *testing.T) {
+		x, y := 7.0, 5.0
+		want := 2.0
+
+		actAssert(x, y, want)
+	})
+
+	t.Run("negative input", func(t *testing.T) {
+		x, y := -7.0, -5.0
+		want := -2.0
+
+		actAssert(x, y, want)
+	})
 }
 
 func TestMultiply(t *testing.T) {
 	e := calculator.Engine{}
-	x, y := 2.0, 5.0
-	want := 10.0
 
-	got := e.Multiply(x, y)
+	actAssert := func(x, y, want float64) {
+		got := e.Multiply(x, y)
 
-	if got != want {
-		t.Errorf("Multiply(%.2f,%.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
+		if got != want {
+			t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
+		}
 	}
+
+	t.Run("positive input", func(t *testing.T) {
+		x, y := 2.0, 5.0
+		want := 10.0
+
+		actAssert(x, y, want)
+	})
+
+	t.Run("negative input", func(t *testing.T) {
+		x, y := -2.0, -5.0
+		want := 10.0
+
+		actAssert(x, y, want)
+	})
 }
 
 func TestDivide(t *testing.T) {
 	e := calculator.Engine{}
-	x, y := 10.0, 2.0
-	want := 5.0
 
-	got := e.Divide(x, y)
+	actAssert := func(x, y, want float64) {
+		got := e.Divide(x, y)
 
-	if got != want {
-		t.Errorf("Divide(%.2f,%.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
+		if got != want {
+			t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
+		}
 	}
+
+	t.Run("positive input", func(t *testing.T) {
+		x, y := 10.0, 2.0
+		want := 5.0
+
+		actAssert(x, y, want)
+	})
+
+	t.Run("negative input", func(t *testing.T) {
+		x, y := -10.0, -2.0
+		want := 5.0
+
+		actAssert(x, y, want)
+	})
 }
