5976aba261bf5b4a55d77b7cac5e8c401e188b72
willbrid <ngaswilly77@gmail.com>
2024-11-18 04:41:35 +0100

    feat: define operation struct

--- 33-tdd/calculator-project/calculator/engine.go
+++ 33-tdd/calculator-project/calculator/engine.go
@@ -2,6 +2,12 @@ package calculator
 
 type Engine struct{}
 
+type Operation struct {
+	Expression string
+	Operator   string
+	Operands   []float64
+}
+
 func (e *Engine) Add(x, y float64) float64 {
 	return x + y
 }
