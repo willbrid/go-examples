54d25433ef64a00196a7bee29d72b5a7d04abbca
willbrid <ngaswilly77@gmail.com>
2025-03-09 15:22:44 +0100

    feat: add testing contructor example

---
+++ 30-power-of-go-tests/3-thing/go.mod
@@ -0,0 +1,5 @@
+module thing
+
+go 1.23.7
+
+require github.com/google/go-cmp v0.7.0 // indirect
---
+++ 30-power-of-go-tests/3-thing/thing_test.go
@@ -0,0 +1,27 @@
+package thing_test
+
+import (
+	"testing"
+
+	"thing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNewThing_ReturnsThingWithGivenXYZValues(t *testing.T) {
+	t.Parallel()
+
+	x, y, z := 1, 2, 3
+	want := &thing.Thing{
+		X: x,
+		Y: y,
+		Z: z,
+	}
+	got, err := thing.NewThing(x, y, z)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
