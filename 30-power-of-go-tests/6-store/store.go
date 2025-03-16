38bfef517ebf131088cb82bf4a5cd22f03d69398
willbrid <ngaswilly77@gmail.com>
2025-03-16 09:21:35 +0100

    docs: add example with sentinel error

--- 30-power-of-go-tests/6-store/store.go
+++ 30-power-of-go-tests/6-store/store.go
@@ -1,15 +1,30 @@
 package store
 
-import "os"
+import (
+	"errors"
+	"os"
+)
+
+var ErrUnopenable error = errors.New("can't open store file")
 
 type Store struct{}
 
 func Open(path string) (*Store, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return nil, ErrUnopenable // perte d'informations sur err
 	}
 
 	f.Close()
 	return &Store{}, nil
 }
+
+// func Open(path string) (*Store, error) {
+// 	 f, err := os.Open(path)
+// 	 if err != nil {
+// 		return nil, err
+// 	 }
+
+// 	 f.Close()
+// 	 return &Store{}, nil
+// }
--- 30-power-of-go-tests/6-store/store_test.go
+++ 30-power-of-go-tests/6-store/store_test.go
@@ -9,6 +9,8 @@ import (
 )
 
 func TestOpen_ReturnsStoreObject(t *testing.T) {
+	t.Parallel()
+
 	want := &store.Store{}
 	got, err := store.Open("testdata/godlen.txt") // Le chemin correct du fichier est : testdata/golden.txt
 
@@ -20,5 +22,20 @@ func TestOpen_ReturnsStoreObject(t *testing.T) {
 	}
 }
 
+func TestOpen_GivesErrUnopenableForBogusFile(t *testing.T) {
+	t.Parallel()
+
+	_, err := store.Open("bogus")
+	if err != store.ErrUnopenable {
+		t.Errorf("wrong error: %v", err)
+	}
+}
+
 // Lors des tests, ignorer les erreurs est une erreur. Aussi par rapport à un test sur un comportement précis si une erreur survient
 // alors il faut utiliser la fonction t.Fatalf pour stopper immédiatement le test.
+
+// 1- Si une erreur est destinée à signaler quelque chose à l'utilisateur, une erreur sentinelle ne sera probablement pas
+// très utile, car sa valeur est fixe. Nous ne pouvons pas l'utiliser pour transmettre des informations dynamiques susceptibles d'aider
+// l'utilisateur à résoudre le problème.
+// 2- Si le système n'a besoin de rien savoir de l'erreur, si ce n'est qu'elle n'est pas nulle, alors une erreur sentinelle n'est pas
+// nécessaire du tout.
