fc4f7ea4fe8435665411a4b92cec7d1f8e9815c7
willbrid <ngaswilly77@gmail.com>
2025-03-25 04:19:48 +0100

    docs: add a comment for dealing with with cross‐platform line endings

--- 30-power-of-go-tests/12-tps/tps_test.go
+++ 30-power-of-go-tests/12-tps/tps_test.go
@@ -31,3 +31,11 @@ func TestWriteReportFile_ProducesCorrectOutputFile(t *testing.T) {
 // Les fichiers sont souvent utilisés comme fichiers de référence dans les tests.
 // Un fichier de référence contient la sortie attendue d'un processus, et nous pouvons la comparer à la sortie réelle
 // pour vérifier son exactitude.
+
+// Gestion des fins de ligne multiplateformes
+// Une solution consiste à ajouter un fichier .gitattributes à la racine de notre référentiel ou dans le repertoire testdata,
+// avec le contenu suivant : * -text
+// Ceci indique à Git de traiter tous les fichiers (*) du référentiel ou du repertoire testdata comme non textuels (-text).
+// Autrement dit, de traiter tous les fichiers comme binaires, sans traduction des fins de ligne.
+// Cela devrait résoudre le problème, et les utilisateurs disposant de versions à jour de Windows et d'éditeurs de texte
+// comme le Bloc-notes pourront facilement modifier les fichiers s'ils le souhaitent.
