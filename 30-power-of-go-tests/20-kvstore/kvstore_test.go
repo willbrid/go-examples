067671c3d877877241db13893f48061c6b8fc80d
willbrid <ngaswilly77@gmail.com>
2025-05-09 11:29:11 +0200

    docs: add example of writing concurrent safety

---
+++ 30-power-of-go-tests/20-kvstore/kvstore_test.go
@@ -0,0 +1,28 @@
+package kvstore_test
+
+import (
+	"runtime"
+	"strconv"
+	"sync"
+	"testing"
+
+	"kvstore"
+)
+
+func TestKVStore_HasNoDataRace(t *testing.T) {
+	t.Parallel()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	s := kvstore.NewStore()
+	go func() {
+		for i := range 1000 {
+			s.Set("foo", strconv.Itoa(i))
+		}
+		wg.Done()
+	}()
+	for range 1000 {
+		_ = s.Get("foo")
+		runtime.Gosched()
+	}
+	wg.Wait()
+}
