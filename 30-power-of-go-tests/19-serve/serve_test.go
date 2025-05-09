0704224229926e9d8d3c377ea61637749f928c02
willbrid <ngaswilly77@gmail.com>
2025-05-09 11:07:56 +0200

    docs: add example for waiting async server to start with a timeout defined

---
+++ 30-power-of-go-tests/19-serve/serve_test.go
@@ -0,0 +1,43 @@
+package serve_test
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"serve"
+)
+
+func TestListenAsync_ListensWithinReasonableTime(t *testing.T) {
+	t.Parallel()
+	addr := randomLocalAddr(t)
+	serve.ListenAsync(addr)
+	waitForServer(t, addr)
+}
+
+func randomLocalAddr(t *testing.T) string {
+	t.Helper()
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
+	return l.Addr().String()
+}
+
+func waitForServer(t *testing.T, addr string) {
+	t.Helper()
+	timeout := time.NewTimer(100 * time.Millisecond)
+	defer timeout.Stop()
+	_, err := net.Dial("tcp", addr)
+	for err != nil {
+		select {
+		case <-timeout.C:
+			t.Fatal("timed out")
+		default:
+			t.Log("retrying: ", err)
+			time.Sleep(time.Millisecond)
+			_, err = net.Dial("tcp", addr)
+		}
+	}
+}
