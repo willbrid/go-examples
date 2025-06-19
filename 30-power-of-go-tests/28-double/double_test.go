4be38136e399eb561f947176a790cd63f4a90a2c
willbrid <ngaswilly77@gmail.com>
2025-06-19 05:53:41 +0200

    docs: add example for testscript as tests concept

---
+++ 30-power-of-go-tests/28-double/double_test.go
@@ -0,0 +1,16 @@
+package double_test
+
+import (
+	"testing"
+
+	"double"
+)
+
+func TestDouble2Returns4(t *testing.T) {
+	t.Parallel()
+	want := 4
+	got := double.Double(2)
+	if want != got {
+		t.Errorf("Double(2): want %d, got %d", want, got)
+	}
+}
---
+++ 30-power-of-go-tests/28-double/go.mod
@@ -0,0 +1,3 @@
+module double
+
+go 1.23.10
