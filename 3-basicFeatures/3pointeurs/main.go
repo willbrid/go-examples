34893349a14bb2cdd9e7d09dd80591dc7321f5b0
willbrid <ngaswilly77@gmail.com>
2025-12-13 16:25:00 +0100

    docs: add comment

--- 3-basicFeatures/3pointeurs/main.go
+++ 3-basicFeatures/3pointeurs/main.go
@@ -6,28 +6,29 @@ import (
 )
 
 func main() {
-	/** Utilisation des pointeurs **/
+	/** Utilisation des pointeurs
+	Un pointeur est une variable dont sa valeur est une adresse mémoire.
+	**/
 
+	/**
+	Le type d'un pointeur dépend du type de la variable dont il est issu, précédé d'un astérisque (*).
+	La variable nommée « second » est de type *int, car elle a été créée en appliquant l'opérateur d'adresse (&) à la variable nommée « first »,
+	dont la valeur est un entier (int). Le type *int indique qu'il s'agit d'une variable dont la valeur est une adresse mémoire
+	stockant un entier.
+
+	L'expression "suivre un pointeur" signifie lire la valeur à l'adresse mémoire pointée par le pointeur, et elle est effectuée à
+	l'aide d'un astérisque (le caractère *).
+	**/
 	var first int = 100
 	var second *int = &first
 
 	first++
 	*second++
-
-	/*
-		Les chaines de caractères précédées d'une barre oblique inverse sont interprétées si la valeur est placée entre guillemets doubles
-		"Bonjour\n". Les séquences d'échappement ne sont pas interprétées si la valeur est placée entre guillemets obliques inverses
-		`Bonjour\n`.
-
-		Les caractères, les glyphes et les séquences d'échappement sont entourés de guillemets
-		simples (le caractère ').
-	*/
 	fmt.Println("First : ", first)
 	fmt.Println("Second : ", second)
 	fmt.Println("Second- : ", *second)
 
-	var myNewPointer *int
-	myNewPointer = second
+	var myNewPointer *int = second
 	*myNewPointer++
 
 	fmt.Println("First : ", first)
@@ -36,7 +37,8 @@ func main() {
 	var first1 int = 100
 	var second1 *int
 
-	fmt.Println(second1)
+	// Les pointeurs définis mais auxquels aucune valeur n'a été attribuée ont la valeur zéro nil
+	fmt.Println(second1) // nil
 	second1 = &first1
 	fmt.Println(second1)
 
