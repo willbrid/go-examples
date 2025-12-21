a2c4e85e6e32c6cb42d9607a987d1e048dd832ca
willbrid <ngaswilly77@gmail.com>
2025-12-21 23:31:42 +0100

    docs: add closure concept

--- 8-functionTypes/2syntaxe-literale-de-fonctions/functions.go
+++ 8-functionTypes/2syntaxe-literale-de-fonctions/functions.go
@@ -53,3 +53,67 @@ func selectCalculator1(price float64) calcFunc {
 		return price
 	}
 }
+
+/**
+Les fonctions définies à l'aide de la syntaxe littérale peuvent faire référence à des variables du code environnant,
+une fonctionnalité connue sous le nom de `closure`.
+
+La fonction `priceCalcFactory` a pour rôle de créer des fonctions de calcul pour une combinaison spécifique de `threshold` et de `rate`.
+Ses entrées sont le seuil et le taux d'une catégorie, et sa sortie est une fonction qui calcule les prix appliqués à cette catégorie.
+Le code de la fonction `priceCalcFactory` utilise la syntaxe littérale pour définir une fonction de calcul contenant le code commun
+nécessaire au calcul.
+
+La fonctionnalité closure assure la liaison entre la fonction `priceCalcFactory` et la fonction de calcul.
+Cette dernière utilise deux variables pour produire un résultat.
+La fonctionnalité closure permet à une fonction d'accéder aux variables et paramètres du code environnant.
+Ici, la fonction de calcul utilise les paramètres de la fonction `priceCalcFactory`. Lorsqu'elle est appelée, les valeurs de
+ces paramètres servent à calculer le résultat.
+**/
+
+func priceCalcFactory(threshold, rate float64) calcFunc {
+	return func(price float64) float64 {
+		if price > threshold {
+			return price + price*rate
+		}
+
+		return price
+	}
+}
+
+/**
+Les variables sur lesquelles la fonction de calcul est appelée sont évaluées à chaque appel de la fonction,
+ce qui signifie que des modifications effectuées en dehors de la fonction peuvent affecter les résultats qu'elle produit.
+
+Lors de l'appel à la fonction de calcul, la valeur de zeroPrices est copiée et il ne s'agit pas toujours de sa valeur courante.
+**/
+
+func priceCalcFactoryWithZeroPrices(threshold, rate float64, zeroPrices bool) calcFunc {
+	return func(price float64) float64 {
+		if zeroPrices {
+			return 0
+		} else if price > threshold {
+			return price + price*rate
+		} else {
+			return price
+		}
+	}
+}
+
+/**
+La plupart des problèmes liés au closure sont dus à des modifications apportées aux variables après la création d'une fonction.
+Ces problèmes peuvent être résolus à l'aide de l'utilisation de pointeur qui empêche la copie des valeurs.
+
+Lors de l'appel à la fonction de calcul, c'est la valeur courante du pointeur zeroPrices qui sera utilisée.
+**/
+
+func priceCalcFactoryWithZeroPricesPointer(threshold, rate float64, zeroPrices *bool) calcFunc {
+	return func(price float64) float64 {
+		if *zeroPrices {
+			return 0
+		} else if price > threshold {
+			return price + price*rate
+		} else {
+			return price
+		}
+	}
+}
--- 8-functionTypes/2syntaxe-literale-de-fonctions/main.go
+++ 8-functionTypes/2syntaxe-literale-de-fonctions/main.go
@@ -23,4 +23,43 @@ func main() {
 			return price + price*0.2
 		})
 	}
+
+	watersportsProducts := map[string]float64{
+		"Kayak":      275,
+		"Lifejacket": 48.95,
+	}
+	soccerProducts := map[string]float64{
+		"Soccer Ball": 19.50,
+		"Stadium":     79500,
+	}
+	waterCalc := priceCalcFactory(100, 0.2)
+	soccerCalc := priceCalcFactory(50, 0.1)
+	for product, price := range watersportsProducts {
+		printPrice(product, price, waterCalc)
+	}
+	for product, price := range soccerProducts {
+		printPrice(product, price, soccerCalc)
+	}
+
+	var prizeGiveaway bool = false
+	waterCalc1 := priceCalcFactoryWithZeroPrices(100, 0.2, prizeGiveaway)
+	prizeGiveaway = true
+	soccerCalc1 := priceCalcFactoryWithZeroPrices(50, 0.1, prizeGiveaway)
+	for product, price := range watersportsProducts {
+		printPrice(product, price, waterCalc1)
+	}
+	for product, price := range soccerProducts {
+		printPrice(product, price, soccerCalc1)
+	}
+
+	var prizeGiveaway1 bool = false
+	waterCalc2 := priceCalcFactoryWithZeroPricesPointer(100, 0.2, &prizeGiveaway1)
+	prizeGiveaway1 = true
+	soccerCalc2 := priceCalcFactoryWithZeroPricesPointer(50, 0.1, &prizeGiveaway1)
+	for product, price := range watersportsProducts {
+		printPrice(product, price, waterCalc2)
+	}
+	for product, price := range soccerProducts {
+		printPrice(product, price, soccerCalc2)
+	}
 }
