4934a7d84d9d8454e2b656dbf4b016cb906460a8
willbrid <ngaswilly77@gmail.com>
2025-12-25 20:51:15 +0100

    docs: remove unused files

--- 11-packages/fmt/formats.go
+++
@@ -1,7 +0,0 @@
-package fmt
-
-import "strconv"
-
-func ToCurrency(amount float64) string {
-	return "$" + strconv.FormatFloat(amount, 'f', 2, 64)
-}
--- 11-packages/greet/hello.go
+++
@@ -1,5 +0,0 @@
-package greet
-
-func GetHello(name string /* valeur par défaut : World */) string {
-	return "Hello " + name
-}
